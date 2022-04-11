7bd4c84b2a81a94d356fdd62f22e241f3ee136f9
池田将臣 <maple.consult00@gmail.com>
2022-04-11 18:59:49 +0900

    boards_controllerの修正 forum model の追加 マイグレーションファイルの作成 forumページの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_11_073547) do
+ActiveRecord::Schema.define(version: 2022_04_11_092929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bbs", force: :cascade do |t|
+  create_table "forums", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
