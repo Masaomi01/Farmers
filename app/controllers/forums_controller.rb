04d4a6efdfcdbd75f2a077c1d7206a441295c79b
池田将臣 <maple.consult00@gmail.com>
2022-04-13 18:22:33 +0900

    投稿詳細ページの作成 headerの固定化

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,7 +1,14 @@
 class ForumsController < ApplicationController
+    before_action :set_forum, only: [:show]
+    
     def index
         @forums = Forum.all
     end
     def show
+    end
+    private
+
+    def set_forum
+        @forum = Forum.find(params[:id])
     end
 end
